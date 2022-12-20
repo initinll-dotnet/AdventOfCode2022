9d3d6654a8e2d8533bc08caa8d20cb15bf770b5f
Nitin Londhe <nitin.londhe@genmills.com>
2022-12-21 02:19:01 +0530

    refactored clean naming

--- Day5/Solution.cs
+++ Day5/Solution.cs
@@ -23,53 +23,72 @@ public class Solution
         if (stacks.Contains("\t"))
             throw new Exception("Tabs not allowed in input, use only space");
 
-        var data = GetStackCrates(stacks);                
+        if (instructions.Contains("\t"))
+            throw new Exception("Tabs not allowed in input, use only space");
+
+        var stackColumns = GetStackColumns(stacks);
+
+        var stackRows = GetStackRows(stacks);
+
+        var data = GetStackCrates(stackColumns, stackRows);                
     }
 
-    private static string[,] GetStackCrates(string stacks)
+    private static int GetStackColumns(string stacks)
     {
         // calculate no of stack columns
-        var stackColumnsCount = stacks
+        var stackColumns = stacks
                 .Split("\r\n")  // split stack set by newline
                 .Last()         // get the last line having stack nos
                 .Split(" ")     // split the last line by space
                 .Where(s => !string.IsNullOrEmpty(s)) // only get nos
-                .Count(); // get total stack count
+                .ToList()
+                .AsEnumerable();
 
+        return stackColumns.Count();
+    }
+
+    private static IEnumerable<string[]> GetStackRows(string stacks)
+    {
         // calculate no of crate rows
-        var crateRows = stacks
+        var stackRows = stacks
                 .Split("\r\n")  // split stack set by newline
                 .SkipLast(1)
                 .Select(s => s.Split(" "))
                 .ToList();
 
-        var crateRowsCount = crateRows.Count;
+        return stackRows;
+    }
 
-        int emptySpaceBetweenEachCrateRow = 4;
+    private static string[,] GetStackCrates(int stackColumns, IEnumerable<string[]> stackRows)
+    {
+        var stackRowsCount = stackRows.Count();
+        var stackColumnsCount = stackColumns;
 
-        string[,] stackCrates = new string[crateRowsCount, stackColumnsCount];
+        int emptySpaceBetweenEachStack = 4;
 
-        for (int crateRowIndex = 0; crateRowIndex < crateRowsCount; crateRowIndex++)
+        string[,] stackCrates = new string[stackRowsCount, stackColumnsCount];
+
+        for (int stackRowIndex = 0; stackRowIndex < stackRowsCount; stackRowIndex++)
         {
-            var currentCrateRow = crateRows[crateRowIndex];
+            var currentStackRow = stackRows.ElementAt(stackRowIndex);
             var currentCrateIndex = 0;
-            var currentCrateRowTotalIndex = currentCrateRow.Count() - 1;
+            var currentStackRowTotalIndex = currentStackRow.Count() - 1;
 
             for (int stackColumnIndex = 0; stackColumnIndex < stackColumnsCount; stackColumnIndex++)
             {
-                if (currentCrateIndex > currentCrateRowTotalIndex)
+                if (currentCrateIndex > currentStackRowTotalIndex)
                 {
                     break;
                 }
 
-                if (currentCrateRow[currentCrateIndex] != "")
+                if (currentStackRow[currentCrateIndex] != "")
                 {
-                    stackCrates[crateRowIndex,stackColumnIndex] = currentCrateRow[currentCrateIndex];
+                    stackCrates[stackRowIndex,stackColumnIndex] = currentStackRow[currentCrateIndex];
                     currentCrateIndex++;
                     continue;
                 }
 
-                currentCrateIndex += emptySpaceBetweenEachCrateRow;
+                currentCrateIndex += emptySpaceBetweenEachStack;
             } 
         }
         
